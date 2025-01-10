ef77375ef74e41e9684c8d192bb47b70e7597b3c
ivossos <johnvossos2001@gmail.com>
2025-01-10 18:17:03 +0200

    Poopoo

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"lemin/lemin"
 	"os"
 )
 
 func main() {
 	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run . [mazename.txt]")
 		return
 	}
 	lemin.Run(os.Args[1])
